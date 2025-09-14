875f9d10af4cea0fb5f833dff3339cb86fd39f6c
Rahul810-dev <rahul.810.n@gmail.com>
2025-09-14 23:49:41 +0530

    ll

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Mynavbar from '/src/components/Mynavbar.jsx';
-import Footer from '/src/components/footer.jsx';
-import Request_Help from '/src/components/Request_Help.jsx';
-import App from '/src/App.jsx'; 
-import Mymap from '/src/components/Mymap.jsx';
-import About from '/src/components/About.jsx';
+import Mynavbar from './components/Mynavbar.jsx';
+import Footer from './components/Footer.jsx';
+import Request_Help from './components/Request_Help.jsx';
+import App from './App.jsx'; 
+import Mymap from './components/Mymap.jsx';
+import About from './components/About.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
